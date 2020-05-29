31d1b6dacff8d574b832f058fca338e67e40667e
christosdemosthenous <43947918+christosdemosthenous@users.noreply.github.com>
2020-05-29 12:13:45 +0300

    Update html-processor.py

--- html-processor.py
+++ html-processor.py
@@ -14,22 +14,17 @@ def myfunction(m):
     return ' '	
 
 rexp1 = re.compile('<title>(.+?)</title>')	
-
 rexp2 = re.compile('<!--.*?-->',re.DOTALL)  
 rexp3 = re.compile(r'<(s(?:cript|tyle)).*?>.*?</\1>',re.DOTALL) 
-
 rexp4 = re.compile(r'<a.+?href="(.*?)".*?>(.*?)</a>',re.DOTALL)
-
-rexp5_1 = re.compile(r'<.+?>|</.+?>',re.DOTALL) 
-rexp5_2 = re.compile(r'<.+?/>',re.DOTALL) 
-
-rexp6 = re.compile(r'&(amp|gt|lt|nbsp);')
-
-rexp7 = re.compile(r'\s+') 
+rexp5p1 = re.compile(r'<.+?>|</.+?>',re.DOTALL) 
+rexp5p2 = re.compile(r'<.+?/>',re.DOTALL) 
+rex_6 = re.compile(r'&(amp|gt|lt|nbsp);')
+rex_7 = re.compile(r'\s+') 
 
 with open('testpage.txt','r') as fp:
 
-  text = fp.read()
+  a = fp.read()
 
   m = rexp1.search(text) 
   
@@ -41,11 +36,11 @@ with open('testpage.txt','r') as fp:
 
   for m in rexp4.finditer(text):
     print('{}    {}'.format(m.group(1),m.group(2)))
-  text = rexp5_1.sub(' ',text) 
-  text = rexp5_2.sub(' ',text) 
+  text = rexp5p1.sub(' ',text) 
+  text = rexp5p2.sub(' ',text) 
 
-  text = rexp6.sub(myfunction,text)
+  text = rex_6.sub(myfunction,text)
 
-  text = rexp7.sub(' ',text) 
+  text = rex_7.sub(' ',text) 
 
   print(text) 
